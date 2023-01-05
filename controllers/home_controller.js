3ad42526f04f447ab833cc69065f23b4a68ad598
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-06 00:54:47 +0530

    add a controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+
+module.exports.home = function(req,res){
+    res.end('<h1>Hello welcome to first express contoller action</h1>');
+}
+
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+app.use('/',require('./routes/index'));
+
 app.listen(port,function(error){
     if(error){
         console.log(`Error : error in running express server ${error}`);
     }
 
     console.log(`Server is Up and Running on port : ${port}`);
-    
+
 });
