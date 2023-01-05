e6d754215e222f73886e1c3edfee2a5236111655
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-05 20:49:52 +0530

    First commit:Create directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(error){
+    if(error){
+        console.log(`Error : error in running express server ${error}`);
+    }
+
+    console.log(`Server is Up and Running on port : ${port}`);
+    
+});
