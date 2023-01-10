1f97117f934007a9334c084aac423378d74bb65c
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-10 21:51:30 +0530

    created partials for header and footer and use it in home and profile.ejs file-learning partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>Users Profile Controller');
+    // return res.end('<h1>Users Profile Controller');
+
+    return res.render('user_profile',{
+        title:'Users Home'
+    });
 }
