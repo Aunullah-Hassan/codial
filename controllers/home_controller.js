611fec054b7f4f930f7b09ccc97170e53371e014
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-10 17:11:35 +0530

    render my ejs file home.ejs from home action of home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 
 module.exports.home = function(req,res){
-    res.end('<h1>Hello welcome to first express contoller action</h1>');
+   //  res.end('<h1>Hello welcome to first express contoller action</h1>');
+
+   return res.render('home',{
+      title: "Home"
+   });
 }
 
