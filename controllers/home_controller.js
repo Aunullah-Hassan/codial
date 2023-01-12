a8b79d61554a5f4eaaac435568dd0fbe3556e253
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-12 07:28:28 +0530

    understanding cookie by creating and altering cookie on both client and server side

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,6 +2,8 @@
 module.exports.home = function(req,res){
    //  res.end('<h1>Hello welcome to first express contoller action</h1>');
 
+   console.log(req.cookies);
+   res.cookie('user_id',25)
    return res.render('home',{
       title: "Home"
    });
