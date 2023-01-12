5863a9b964c1e4af1d9c543b175ad2e17187c082
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-12 05:47:50 +0530

    Render pages for signUp and signIn y vreating action in user controler and create signup,signin forms

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,27 @@ module.exports.profile=function(req,res){
         title:'Users Home'
     });
 }
+
+// Render the sign Up Page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codial | Sign Up"
+    });
+}
+
+// Render the Sign In Page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codial | Sign In"
+    });
+}
+
+// Get the Sign up data
+module.exports.create=function(req,res){
+    // code later
+}
+
+// Sign In and create the session for the user
+module.exports.signIn=function(req,res){
+    // code later
+}
