17efa45779c81c299a8131140b9a8ab1692e0ad0
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-17 05:50:48 +0530

    display posts and related user information

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,33 @@
 
+const Post=require('../models/post');
+
 module.exports.home = function(req,res){
    //  res.end('<h1>Hello welcome to first express contoller action</h1>');
 
-   console.log(req.cookies);
-   res.cookie('user_id',25)
-   return res.render('home',{
-      title: "Home"
-   });
+   // console.log(req.cookies);
+   // res.cookie('user_id',25)
+
+   // Post.find({},function(err,posts){
+   //    if(err){
+   //       console.log('error in displaying posts');
+   //       return;
+   //    }
+   //    return res.render('home',{
+   //       title: "Codial | Home",
+   //       posts:posts
+   //    });
+   // });
+
+   // pre-Populating posts with corrresponding users
+   Post.find({}).populate('user').exec(function(err,posts){
+
+         return res.render('home',{
+         title: "Codial | Home",
+         posts:posts
+      });
+
+   })
+
+   
 }
 
