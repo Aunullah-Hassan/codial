cb58409951f463c4f0f5c9fa2ccae5222d81c165
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-18 06:06:32 +0530

    delete a post and all comments on it from db with 3 Levels of Auth Checks

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
 
 module.exports.create=function(req,res){
 
@@ -15,4 +16,23 @@ module.exports.create=function(req,res){
 
     });
     
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+
+        // Check if one deleting the post is same as the One who have created the post(3rd level Authorization)
+        if(post.user == req.user.id){
+            // .id means converting the object id into string
+            
+            post.remove();
+
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
