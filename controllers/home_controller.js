fca7904666b5df935bc165143c20f58d7d740ff4
Aunullah Hassan <hassanmdaunullah@gmail.com>
2023-01-19 22:24:12 +0530

    Converting to async await,home_controller.home--action

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@
 const Post=require('../models/post');
 const User=require('../models/user');
 
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
    //  res.end('<h1>Hello welcome to first express contoller action</h1>');
 
    // console.log(req.cookies);
@@ -20,28 +20,56 @@ module.exports.home = function(req,res){
    // });
 
    // pre-Populating posts with corrresponding users
-   Post.find({})
-   .populate('user')
-   .populate({
-      path:'comments',
-      populate:{
-         path:'user'
-      }
-   })
-   .exec(function(err,posts){
+               // Post.find({})
+               // .populate('user')
+               // .populate({
+               //    path:'comments',
+               //    populate:{
+               //       path:'user'
+               //    }
+               // })
+               // .exec(function(err,posts){
 
-      User.find({},function(err,users){
+               //    User.find({},function(err,users){
 
-         return res.render('home',{
-            title: "Codial | Home",
-            posts:posts,
-            all_users:users
-         });
+               //       return res.render('home',{
+               //          title: "Codial | Home",
+               //          posts:posts,
+               //          all_users:users
+               //       });
 
+               //    });
+                     
+
+               // })
+
+               // ABOVE CODE IS CALLBACK HELL WITHOUT ASYNC AWAIT
+
+   try{
+
+      let posts=await Post.find({})
+      .populate('user')
+      .populate({
+         path:'comments',
+         populate:{
+            path:'user'
+         }
       });
-         
+   
+      let users= await User.find({});
+   
+      return res.render('home',{
+         title: "Codial | Home",
+         posts:posts,
+         all_users:users
+      });
 
-   })
+   }catch(err){
+      console.log('Error :',err);
+      return;
+   }
+
+
 
    
 }
